fb6a055de44cf5056ca8af41a9cbc6ab100f900e
shaneis <shaneoneillis@gmail.com>
2019-05-10 09:59:14 +0100

    Hiding output.

--- hashset.ps1
+++ hashset.ps1
@@ -11,13 +11,18 @@ $HashSet = [System.Collections.Generic.HashSet[Int]]::new()
 foreach ($item in $Boom,$Blast,$And,$Ruin) {
     $HashSet.Add($Item)
 }
-
-
 $HashSet
 
 # Duplicate time.
 $HashSet.Clear()
 foreach ($item in $Boom,$Blast,$And,$Boom) {
     $HashSet.Add($item)
+}
+$HashSet
+
+#Hide the output
+$HashSet.Clear()
+$null = foreach ($item in $Boom,$Blast,$And,$Ruin) {
+    $HashSet.Add($Item)
 }
 $HashSet
