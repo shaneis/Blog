9407159d6b5fcdbcdb42899ed2c0a0b247705866
shaneis <shaneoneillis@gmail.com>
2019-07-29 13:37:29 +0100

    initial commit of helper function

---
+++ PSScriptRootVersusDot/Get-Name.ps1
@@ -0,0 +1,14 @@
+function Get-Name {
+    [CmdletBinding()]
+    param (
+        [Parameter(Position = 0)]
+        [String]
+        $Name
+    )
+
+    begin {}
+
+    process {}
+
+    end {}
+}
