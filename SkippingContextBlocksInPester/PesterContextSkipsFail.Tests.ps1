cdec6a4b46fe79b80ea13d767ca25543512199aa
shaneis <shaneoneillis@gmail.com>
2019-09-17 10:58:10 +0100

    Added Context tags

--- SkippingContextBlocksInPester/PesterContextSkipsFail.Tests.ps1
+++ SkippingContextBlocksInPester/PesterContextSkipsFail.Tests.ps1
@@ -1,11 +1,11 @@
-Describe -Name 'Context Skipping' -Tag SkipThis -Fixture {
-    Context -Name 'Skip this' -Fixture {
+Describe -Name 'Context Skipping' -Fixture {
+    Context -Name 'Skip this' -Tag SkipThis -Fixture {
         It -Name 'should return 2 when passed 1 + 1' -Test {
             1 - 1 | Should -Be 2
         }
     }
 
-    Context -Name 'Do not skip this' -Fixture {
+    Context -Name 'Do not skip this' -Tag RunThis -Fixture {
         It -Name 'should return 4 when passed 2 + 2' -Test {
             2 + 2 | Should -Be 4
         }
